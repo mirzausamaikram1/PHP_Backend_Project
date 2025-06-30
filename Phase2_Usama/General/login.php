2c427311a3f994c67b17efe31f6c151f38a7d494
Usama IKRAM <240338484@stu.vtc.edu.hk>
2025-06-30 19:06:14 +0300

    Login

--- Phase2_Usama/General/login.php
+++ Phase2_Usama/General/login.php
@@ -9,25 +9,30 @@ $message = ''; // This will show a message if something goes right
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = mysqli_real_escape_string($conn, $_POST['username']); // Get the ID they typed (sid or cid)
     $password = mysqli_real_escape_string($conn, $_POST['password']); // Get the password they typed
+    $role = isset($_POST['role']) ? mysqli_real_escape_string($conn, $_POST['role']) : ''; // Get the selected role
 
-    if (empty($id) || empty($password)) {
-        $error = "Please type your ID and password!";
+    if (empty($id) || empty($password) || empty($role)) {
+        $error = "Please type your ID, password, and select a role!";
     } else {
-        // First, check if they are a staff member using sid
-        $staff_sql = "SELECT sid, spassword, sname, 'staff' AS role FROM staff WHERE sid = '$id'";
-        $staff_result = mysqli_query($conn, $staff_sql);
-        if ($staff_result && mysqli_num_rows($staff_result) > 0) {
-            $user = mysqli_fetch_assoc($staff_result); // Get their details
-            if ($password === $user['spassword']) { // Check if the password is correct
-                $_SESSION['sid'] = $user['sid']; // Save their ID
-                $_SESSION['role'] = $user['role']; // Save their role as 'staff'
-                header("Location: ../General/staffDashboard.php"); // Go to staff dashboard
-                exit;
+        if ($role == 'staff') {
+            // Check if they are a staff member using sid
+            $staff_sql = "SELECT sid, spassword, sname, 'staff' AS role FROM staff WHERE sid = '$id'";
+            $staff_result = mysqli_query($conn, $staff_sql);
+            if ($staff_result && mysqli_num_rows($staff_result) > 0) {
+                $user = mysqli_fetch_assoc($staff_result); // Get their details
+                if ($password === $user['spassword']) { // Check if the password is correct
+                    $_SESSION['sid'] = $user['sid']; // Save their ID
+                    $_SESSION['role'] = $user['role']; // Save their role as 'staff'
+                    header("Location: ../General/staffDashboard.php"); // Go to staff dashboard
+                    exit;
+                } else {
+                    $error = "Wrong password for staff ID: $id!";
+                }
             } else {
-                $error = "Wrong password for staff ID: $id!";
+                $error = "Staff ID not found: $id";
             }
-        } else {
-            // If not staff, check if they are a customer using cid
+        } elseif ($role == 'customer') {
+            // Check if they are a customer using cid
             $customer_sql = "SELECT cid, cpassword, cname, 'customer' AS role FROM customer WHERE cid = '$id'";
             $customer_result = mysqli_query($conn, $customer_sql);
             if ($customer_result && mysqli_num_rows($customer_result) > 0) {
@@ -41,8 +46,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $error = "Wrong password for customer ID: $id!";
                 }
             } else {
-                $error = "User not found with ID: $id"; // Show if we can’t find them
+                $error = "Customer ID not found: $id";
             }
+        } else {
+            $error = "Invalid role selected!";
         }
     }
 }
@@ -52,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <html>
     <head>
         <title>Login</title>
-        <link rel="stylesheet" href="../Staff_css/login.css"> <!-- Add some nice styles -->
+        <link rel="stylesheet" href="../Staff_css/login.css">
     </head>
     <body>
     <div class="content">
@@ -68,7 +75,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <label for="username">ID (Staff ID or Customer ID):</label>
             <input type="text" id="username" name="username" required><br>
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
+            <input type="password" id="password" name="password" required><br>
+            <input type="radio" id="customer" name="role" value="customer" checked> <label for="customer">Customer</label>
+            <input type="radio" id="staff" name="role" value="staff"> <label for="staff">Staff</label><br><br>
             <button type="submit">Login</button>
         </form>
     </div>
